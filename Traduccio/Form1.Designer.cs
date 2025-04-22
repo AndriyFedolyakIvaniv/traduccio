bcdd212e622700235a1e40778d20ff29da6b49c4
AndriyFedolyakIvaniv <anfe949@vidalibarraquer.net>
2025-04-22 08:45:14 +0200

    Agregar archivos de proyecto.

---
+++ Traduccio/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Traduccio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            button2 = new Button();
+            label9 = new Label();
+            label10 = new Label();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            resources.ApplyResources(comboBox1, "comboBox1");
+            comboBox1.Name = "comboBox1";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(textBox1, "textBox1");
+            textBox1.Name = "textBox1";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(textBox2, "textBox2");
+            textBox2.Name = "textBox2";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(button1, "button1");
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(label5, "label5");
+            label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(label6, "label6");
+            label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(label7, "label7");
+            label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(label8, "label8");
+            label8.Name = "label8";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(button2, "button2");
+            button2.Name = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            resources.ApplyResources(label9, "label9");
+            label9.Name = "label9";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(label10, "label10");
+            label10.Name = "label10";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(button2);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Button button2;
+        private Label label9;
+        private Label label10;
+    }
+}
---
+++ Traduccio/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Traduccio
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            // Aquí es pot forçar un idioma per defecte si vols:
+            // Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            InitializeComponent();
+            InitializeLanguageComboBox();
+
+        }
+
+        private void InitializeLanguageComboBox()
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Català");
+            comboBox1.Items.Add("Español");
+            comboBox1.Items.Add("English");
+            comboBox1.Items.Add("Français");
+            comboBox1.Items.Add("Deutsch");
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string selectedLanguage = "";
+
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Català":
+                    selectedLanguage = "ca-ES";
+                    break;
+                case "Español":
+                    selectedLanguage = "es-ES";
+                    break;
+                case "English":
+                    selectedLanguage = "en-US";
+                    break;
+                case "Français":
+                    selectedLanguage = "fr-FR";
+                    break;
+                case "Deutsch":
+                    selectedLanguage = "de";
+                    break;
+            }
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(selectedLanguage);
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
